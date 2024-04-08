f1204ae3e3631dd2ca902b2b35211e44343ac062
Benja303 <anugamer303@gmail.com>
2024-04-08 18:31:58 -0400

    tests

--- Frontend/src/test/chargeTest.js
+++ Frontend/src/test/chargeTest.js
@@ -1,19 +1,18 @@
 import { Browser, Builder, WebDriver } from "selenium-webdriver";
+import assert from "assert";
+const GREEN_COLOR = "\x1b[32m";
 
-async function loadTest(url, numUsers) {
+async function loadTest(url, numUsers, maxResponseTime) {
     let startTime = 0;
     let endTime = 0;
     let totalTime = 0;
     const drivers = [];
 
     try {
-        // Crear múltiples instancias de WebDriver
         for (let i = 0; i < numUsers; i++) {
             const driver = await new Builder().forBrowser(Browser.EDGE).build();
             drivers.push(driver);
         }
-
-        // Iniciar la carga para todos los usuarios simultáneamente
         startTime = new Date().getTime();
         await Promise.all(
             drivers.map(async (driver, index) => {
@@ -28,22 +27,21 @@ async function loadTest(url, numUsers) {
         console.log(
             `Tiempo total de carga para ${numUsers} usuarios: ${totalTime} ms`
         );
+        assert.ok(
+            totalTime < maxResponseTime,
+            `El tiempo de respuesta excedió ${maxResponseTime} ms.`
+        );
+        console.log(`${GREEN_COLOR}Passed\x1b[0m`);
     } catch (error) {
         console.error("Error:", error);
     } finally {
-        // Cerrar todas las instancias de WebDriver al finalizar
         for (const driver of drivers) {
             await driver.quit();
         }
     }
 }
 
-// URL de la página web a probar
 const url = "http://localhost:4001";
-// Número de usuarios simultáneos
-const numUsers = 10;
-
-// Ejecutar la prueba de carga
-//loadTest(url, numUsers);
-
-
+const numUsers = 5;
+const maxResponseTimeSeconds = 10;
+loadTest(url, numUsers, maxResponseTimeSeconds * 1000);
