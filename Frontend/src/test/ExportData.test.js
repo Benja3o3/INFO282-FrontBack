b698fe346e444fd7098299281c0ebc594b6283e6
Felipe Prieto Ortiz <felipeprietoortiz@FelipePrieto.local>
2024-04-08 23:53:12 -0400

    screenshot

--- Frontend/src/test/ExportData.test.js
+++ Frontend/src/test/ExportData.test.js
@@ -1,38 +1,31 @@
 import { Browser, Builder, By } from "selenium-webdriver";
 import * as fs from "fs";
 
-/* Requisito funciona: Como usuario quiero exportar los datos en un archivo kml */
+/* Requisito funciona: Como usuario quiero exportar los datos en un archiv kml */
 
 async function exportData(url, browser, downloadsPath) {
   let driver = await new Builder().forBrowser(browser).build();
-  // Maximizar la ventana del navegador para asegurar la captura de pantalla completa
   await driver.manage().window().maximize();
-
   let currentDate = new Date();
   let dateString = `${currentDate.getDate()}-${currentDate.getHours()}:${currentDate.getMinutes()}`;
+
   try {
     await driver.get(url);
     await driver.sleep(2000);
-
-    // Hacer clic en el botón de exportación
+    // console.log("Test: ");
     const exportButton = await driver.findElement(
       By.className("bg-buttonblue")
     );
     await exportButton.click();
-
-    // Esperar hasta que se complete la descarga (ajustar este tiempo según sea necesario)
-    await driver.sleep(5000);
-
-    // Tomar una captura de pantalla
     const screenshot = await driver.takeScreenshot();
-
-    // Verificar si el archivo se ha descargado correctamente
-    const files = await fs.promises.readdir(downloadsPath);
     fs.writeFileSync(
       `./src/test/screenshots/${dateString}-${browser.toString()}-ExportData1.png`,
       screenshot,
       "base64"
     );
+    await driver.sleep(5000);
+
+    const files = await fs.promises.readdir(downloadsPath);
 
     if (files.includes("allData.kml")) {
       console.log(`El archivo se descargó correctamente en ${downloadsPath}`);
@@ -49,9 +42,8 @@ async function exportData(url, browser, downloadsPath) {
   } finally {
     await driver.sleep(2000);
     const screenshot = await driver.takeScreenshot();
-    // Guardar la captura de pantalla en un archivo
     fs.writeFileSync(
-      `./src/test/screenshots/${dateString}-${browser.toString()}-ExportData2.png`,
+      `./src/test/screenshots/${dateString}-${browser.toString()}-ExportData1.png`,
       screenshot,
       "base64"
     );
@@ -60,7 +52,11 @@ async function exportData(url, browser, downloadsPath) {
 }
 
 const url = "http://localhost:4001/";
-const browser = Browser.CHROME;
+const browsers = [Browser.CHROME];
+// const browsers = [Browser.CHROME, Browser.FIREFOX, Browser.EDGE];
+
 const downloadsPath = "/Users/felipeprietoortiz/Downloads";
 
-exportData(url, browser, downloadsPath);
+browsers.forEach((browser) => {
+  exportData(url, browser, downloadsPath);
+});
